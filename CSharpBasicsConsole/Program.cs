aad988d7f70217d8a122eb5b998d7734f8fe23e5
dhollifield <deannahollifield@gmail.com>
2023-01-09 21:26:39 -0600

    created a UserInput class

--- CSharpBasicsConsole/Program.cs
+++ CSharpBasicsConsole/Program.cs
@@ -13,44 +13,59 @@ namespace CSharpBasicsConsole
              * Least favorite school subject: Gym
              * 
              */
+            //object userInput = new
+            //{
+            //    Name = "Name",
+            //    FavoriteDay = "Wednesday",
+            //    LeastFavoriteSubject = "Gym",
+            //    Age = 0
+            //};
+
+            UserInput myUserInput = new UserInput();
+
+            //userInput1.Name = "Deanna";
+            //userInput1.FavoriteDayOFWeek = "Friday";
+            //userInput1.LeastFavoriteSubject = "Gym";
 
             Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            myUserInput.Name = Console.ReadLine();
 
-            while (String.IsNullOrWhiteSpace(name))
+            while (String.IsNullOrWhiteSpace(myUserInput.Name))
             {
                 Console.WriteLine("Please enter a valid name: ");
-                name = Console.ReadLine();
+                myUserInput.Name = Console.ReadLine();
             }
             Console.Clear();
 
             Console.Write("What is your favorite day of the week? ");
-            string dayOfWeek = Console.ReadLine();
+            myUserInput.FavoriteDayOfWeek = Console.ReadLine();
 
             //I'm making this comment.
 
-            while (String.IsNullOrWhiteSpace(dayOfWeek))
+            while (String.IsNullOrWhiteSpace(myUserInput.FavoriteDayOfWeek))
             {
                 Console.WriteLine("Please enter a valid day of the week: ");
-                dayOfWeek = Console.ReadLine();
+                myUserInput.FavoriteDayOfWeek = Console.ReadLine();
             }
             Console.Clear();
 
             Console.Write("What is your least favorite subject in school? ");
-            string leastFaveSchoolSubject = Console.ReadLine();
+            myUserInput.LeastFaveSchoolSubject = Console.ReadLine();
 
-            while (String.IsNullOrWhiteSpace(leastFaveSchoolSubject))
+            while (String.IsNullOrWhiteSpace(myUserInput.LeastFaveSchoolSubject))
             {
                 Console.WriteLine("Please enter a valid school subject: ");
-                leastFaveSchoolSubject = Console.ReadLine();
+                myUserInput.LeastFaveSchoolSubject = Console.ReadLine();
             }
             Console.Clear();
 
-            string displayText = @$"Name: {name}
-Favorite Day of the Week: {dayOfWeek}
-Least Favorite School Subject: {leastFaveSchoolSubject}";
+//            string displayText = @$"Name: {myUserInput.Name}
+//Favorite Day of the Week: {myUserInput.FavoriteDayOfWeek}
+//Least Favorite School Subject: {myUserInput.LeastFaveSchoolSubject}";
 
-            Console.Write(displayText);
+            Console.Write(myUserInput);
+            
+            //Console.Write(displayText);
 
             Console.ReadLine();
 
---
+++ CSharpBasicsConsole/UserInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicsConsole
+{
+
+    //class - a blueprint to represent something in your application
+    //all code in a class will be related
+
+    public class UserInput
+    {
+        /*
+            * 
+            * Name: Deanna Hollifield
+            * Favorite day of the week: Wednesday
+            * Least favorite school subject: Gym
+            * 
+            * object userInput = new
+            * { 
+            * 
+            */
+
+
+        public string Name { get; set; }
+        public string? FavoriteDayOfWeek { get; set; }
+        public string? LeastFaveSchoolSubject { get; set; }
+
+        public override string ToString()
+        {
+            return @$"Name: {Name}
+Favorite Day of the Week: {FavoriteDayOfWeek}
+Least Favorite School Subject: {LeastFaveSchoolSubject}";
+        }
+    }
+}
